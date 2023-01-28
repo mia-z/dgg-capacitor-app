7341acaa621f830990ece2cb8104117b538a4997
Ryan <iperish.x@gmail.com>
2023-01-28 17:26:31 +0100

    refactor queries into a single hook instead of 4 separate hooks

--- src/components/StreamContainer.tsx
+++ src/components/StreamContainer.tsx
@@ -4,7 +4,7 @@ import {useQuery} from "react-query";
 import {useBoundStore} from "../hooks/useBoundStore";
 import {HostInfoQuery, StreamInfoQuery} from "../lib/Queries";
 import {parseEmbedLink} from "../lib/Helpers";
-
+import { useInfoQueries } from "../hooks/useInfoQueries";
 
 type StreamContainerProps = {
     height: number,
@@ -12,24 +12,15 @@ type StreamContainerProps = {
 }
 
 export const StreamContainer: FC<StreamContainerProps> = ({ height, width }) => {
-	const { streamInfo, setStreamInfo, hostInfo, setHostInfo, setCurrentEmbed, usingCustomEmbed, currentEmbed, playerIsHidden } = useBoundStore();
+	const { streamInfo, setCurrentEmbed, usingCustomEmbed, currentEmbed, playerIsHidden } = useBoundStore();
+
+	const queries = useInfoQueries();
 
 	useEffect(() => {
 		if (!usingCustomEmbed) {
 			setCurrentEmbed({ platform: "youtube", videoId: streamInfo?.streams.youtube.id as string });
 		}
 	}, [streamInfo, usingCustomEmbed]);
-
-	const streamInfoQuery = useQuery({
-		queryKey: ["streamInfo", streamInfo],
-		queryFn: async () => await StreamInfoQuery(),
-		onSuccess: (result) => {
-			if (result) {
-				setStreamInfo(result);
-			}
-		},
-		refetchInterval: 1000 * 60
-	});
 
     const playerOpts: YouTubeProps["opts"] = {
 		width: "100%",
