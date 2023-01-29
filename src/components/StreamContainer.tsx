1d693a199e8197c6d776e810318d760957e21f70
Ryan <iperish.x@gmail.com>
2023-01-29 22:19:11 +0100

    update available embed containers in the streamcontainer

--- src/components/StreamContainer.tsx
+++ src/components/StreamContainer.tsx
@@ -1,9 +1,6 @@
 import React, { FC, useContext, useEffect, useState } from "react";
 import YouTube, { YouTubeProps} from "react-youtube";
-import {useQuery} from "react-query";
 import {useBoundStore} from "../hooks/useBoundStore";
-import {HostInfoQuery, StreamInfoQuery} from "../lib/Queries";
-import {parseEmbedLink} from "../lib/Helpers";
 import { useInfoQueries } from "../hooks/useInfoQueries";
 
 type StreamContainerProps = {
@@ -12,6 +9,11 @@ type StreamContainerProps = {
 }
 
 export const StreamContainer: FC<StreamContainerProps> = ({ height, width }) => {
+	const playerDimensions = {
+		width: "100%",
+		height: (9/16) * width
+	}
+	
 	const { streamInfo, setCurrentEmbed, usingCustomEmbed, currentEmbed, playerIsHidden } = useBoundStore();
 
 	const queries = useInfoQueries();
@@ -22,14 +24,33 @@ export const StreamContainer: FC<StreamContainerProps> = ({ height, width }) => 
 		}
 	}, [streamInfo, usingCustomEmbed]);
 
-    const playerOpts: YouTubeProps["opts"] = {
-		width: "100%",
-		height: (9/16) * width
-    };
+	switch (currentEmbed.platform) {
+		case "youtube": {		
+			return (
+				<div className={"transition-all"} style={{ width: "100%", height: playerIsHidden ? 0 : (9/16) * width }}>
+					<YouTube videoId={currentEmbed.videoId} opts={playerDimensions} />
+				</div>
+			);
+		}
+		case "twitch": {
+			return (
+				<div className={"transition-all flex flex-col"} style={{ width: "100%", height: playerIsHidden ? 0 : (9/16) * width }}>
+					{/* <iframe src={`https://player.twitch.tv/?channel=${currentEmbed.videoId}&parent=localhost`} height={playerDimensions.height} width={playerDimensions.width} /> */}
+					<div className={"text-white text-center roboto m-auto"}>
+						Twitch embeds do not work with Ionic, <span className={"italic"}>for now.</span><br />This is Twitch's fault.
+					</div>
+				</div>
+			)
+		}
+		case "rumble": {
+			return (
+				<div className={"transition-all"} style={{ width: "100%", height: playerIsHidden ? 0 : (9/16) * width }}>
+					<iframe src={`https://rumble.com/embed/${currentEmbed.videoId}`} {...playerDimensions} />
+				</div>
+			)
+		}
+		default: return <div></div> //TODO: Improve this somewhat
+	}
 
-	return (
-		<div className={"transition-all"} style={{ width: "100%", height: playerIsHidden ? 0 : (9/16) * width }}>
-			<YouTube videoId={currentEmbed.videoId} opts={playerOpts} />
-        </div>
-    );
+
 }
