0afd23940d0e21a1488c836637e7dc61502f40fc
Ryan <iperish.x@gmail.com>
2023-02-12 19:15:06 +0100

    add slice state for pinned messages

--- src/hooks/useBoundStore.ts
+++ src/hooks/useBoundStore.ts
@@ -3,18 +3,18 @@ import { createChatUsersSlice, ChatUsersSlice } from "../slices/ChatUsersSlice";
 import { createChatMessagesSlice, ChatMessagesSlice } from "../slices/ChatMessagesSlice";
 import { createUserInfoSlice, UserInfoSlice } from "../slices/UserInfoSlice";
 import { createStreamInfoSlice, StreamInfoSlice } from "../slices/StreamInfoSlice";
-import { createHostInfoSlice, HostInfoSlice } from "../slices/HostInfoSlice";
 import { createPlayerSlice, PlayerSlice } from "../slices/PlayerSlice";
 import { createPollSlice, PollSlice } from "../slices/PollSlice";
+import { createPinnedMessageSlice, PinnedMessageSlice } from "../slices/PinnedMessageSlice";
 
-export type CombinedSlices = ChatUsersSlice & ChatMessagesSlice & UserInfoSlice & HostInfoSlice & StreamInfoSlice & PlayerSlice & PollSlice;
+export type CombinedSlices = ChatUsersSlice & ChatMessagesSlice & UserInfoSlice & StreamInfoSlice & PlayerSlice & PollSlice & PinnedMessageSlice;
 
 export const useBoundStore = create<CombinedSlices>()((...s) => ({
 	...createChatUsersSlice(...s),
 	...createChatMessagesSlice(...s),
 	...createUserInfoSlice(...s),
 	...createStreamInfoSlice(...s),
-	...createHostInfoSlice(...s),
 	...createPlayerSlice(...s),
 	...createPollSlice(...s),
+	...createPinnedMessageSlice(...s)
 }));
