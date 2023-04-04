6c81314fcd7db6a6fd6c0cd6dca8719ff82a6e46
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-04-05 01:29:05 +0200

    add platform to slices to avoid calling system every time a reference is needed

--- src/slices/SystemSlice.ts
+++ src/slices/SystemSlice.ts
@@ -1,12 +1,15 @@
 import {StateCreator} from "zustand";
 import {CombinedSlices} from "../hooks/useBoundStore";
+import { Capacitor } from "@capacitor/core";
 
 export type SystemSlice = {
 	chatReady: boolean,
-    setChatReady: (ready: boolean) => void
+    setChatReady: (ready: boolean) => void,
+    platform: string
 }
 
 export const createSystemSlice: StateCreator<CombinedSlices, [], [], SystemSlice> = (set) => ({
 	chatReady: false,
-    setChatReady: (ready: boolean) => set(() => ({ chatReady: ready }))
+    setChatReady: (ready: boolean) => set(() => ({ chatReady: ready })),
+    platform: Capacitor.getPlatform()
 });
