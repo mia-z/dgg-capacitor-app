6ec322b633ce5e56c31a2db73349225b7f0be823
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-04-04 20:02:37 +0200

    add image resources and common export

---
+++ src/Images.ts
@@ -0,0 +1,11 @@
+import KickLogoIcon from "./styles/img/KickLogoIcon.png";
+import TwitchLogoIcon from "./styles/img/TwitchLogoIcon.png";
+import RumbleLogoIcon from "./styles/img/RumbleLogoIcon.png";
+import YoutubeLogoIcon from "./styles/img/YoutubeLogoIcon.png";
+
+export {
+    KickLogoIcon,
+    TwitchLogoIcon,
+    RumbleLogoIcon,
+    YoutubeLogoIcon
+}
