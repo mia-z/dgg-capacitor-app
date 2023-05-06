c349136a9ededced441d7e79bb8bde528c5a804c
Ryan Cockram <miaz@Ryans-MacBook-Pro.local>
2023-05-06 17:02:47 +0200

    add text-shadow extension for tw

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,33 @@
 /** @type {import("tailwindcss").Config} */
 
 const colours = require("tailwindcss/colors");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
     content: ["src/**/*.{tsx,ts,js,jsx,html}"],
 	darkMode: "class",
-	plugins: [require("daisyui")],
+	plugins: [
+		require("daisyui"),
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities({
+				"text-shadow": (value) => ({
+					textShadow: value, 
+				}), 
+			}, { 
+				values: theme("textShadow") 
+			})
+		})
+	],
 	daisyui: {
 		themes: ["dark"]
+	},
+	theme: {
+		extend: {
+			textShadow: {
+			  	sm: "0 1px 2px var(--tw-shadow-color)",
+			  	DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+			  	lg: "0 8px 16px var(--tw-shadow-color)",
+			},
+		},
 	}
 }
